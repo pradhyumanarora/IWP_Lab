ff2aa297c80bc933a2e62cbdc2e3918c3b0410a8
pradhyumanarora <pradhyumanarora@gmail.com>
2023-02-19 23:55:08 +0530

    DA1

---
+++ Lab7/index.js
@@ -0,0 +1,127 @@
+const news = {
+  "2023-02-17": {
+    1: {
+      headline:
+        "New Study Finds Drinking Coffee Can Reduce Risk of Heart Disease",
+      content:
+        "A recent study published in the Journal of Cardiology found that consuming moderate amounts of coffee can help reduce the risk of heart disease. Researchers analyzed data from over 300,000 individuals and found that those who drank 1-3 cups of coffee per day had a lower risk of developing heart disease compared to those who drank less than one cup per day. The study also found that consuming more than 3 cups per day did not provide any additional benefits.",
+        image : "https://upload.wikimedia.org/wikipedia/commons/e/e4/Latte_and_dark_coffee.jpg"
+    },
+    2: {
+      headline: "NASA Announces Successful Launch of New Spacecraft",
+      content:
+        "NASA has successfully launched its latest spacecraft, the Orion, which is designed to take humans back to the moon and eventually to Mars. The spacecraft was launched on a United Launch Alliance Delta IV Heavy rocket from Cape Canaveral Space Force Station in Florida. The mission is part of NASA's Artemis program, which aims to establish a sustainable human presence on the moon by 2024.",
+        image : "https://cdn.mos.cms.futurecdn.net/baYs9AuHxx9QXeYBiMvSLU.jpg"
+    },
+    3: {
+      headline: "Apple Unveils New iPhone with 5G Capability",
+      content: "At its annual fall event, Apple unveiled its latest smartphone, the iPhone 14, which includes 5G capability for faster download speeds and improved connectivity. The new iPhone also features a larger screen, improved camera, and longer battery life. Apple CEO Tim Cook called it the most advanced iPhone yet.",
+      image : "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-iphone-13-pro-graphite-2023?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1671646730078"
+    },
+    4: {
+      headline: "Amazon Announces Expansion Plans for Delivery Fleet",
+      content: "Amazon has announced plans to expand its delivery fleet with the purchase of 10,000 electric vehicles from the automaker Rivian. The vehicles are expected to be on the road by 2022 and will help Amazon achieve its goal of becoming carbon neutral by 2040. The company also plans to invest in renewable energy and reduce its packaging waste.",
+      image : "https://images.livemint.com/img/2023/01/05/1600x900/Amazon_1672923106890_1672923107089_1672923107089.jpg"
+    },
+    5: {
+      headline: "Tesla Reports Record Revenue in Q3 2022",
+      content: "Tesla has reported record revenue for the third quarter of 2022, with sales reaching $10.4 billion, up from $8.8 billion in the previous quarter. The company also reported a net income of $1.6 billion, up from $1.1 billion in the previous quarter. Tesla CEO Elon Musk attributed the success to strong demand for the Model Y and Model 3, as well as the launch of new products like the Cybertruck and Semi truck.",
+      image : "https://media.npr.org/assets/img/2023/02/01/gettyimages-1177714952-1--95f350ff5337d36e5f87249be334f9cfe9d0fd2f-s1100-c50.jpg"
+    },
+    6: {
+      headline: "Facebook Announces Plans to Hire 10,000 Workers",
+      content: "Facebook has announced plans to hire 10,000 workers over the next year to help combat hate speech and misinformation on the platform. The new hires will include engineers, content moderators, and data analysts, and will be located in Facebook's offices around the world. CEO Mark Zuckerberg stated that the company is committed to creating a safer and more secure online environment for all users.",
+      image: "https://img.etimg.com/thumb/msid-98054653,width-294,resizemode-4,imgsize-69712/daily-news-and-latest-updates-live-19-february-2023.jpg"
+    }
+  },
+
+  "2023-02-18": {
+    1: {
+        headline:
+          "WHO Declares Global Health Emergency Due to Outbreak of New Virus",
+        content:
+          "The World Health Organization (WHO) has declared a global health emergency due to the outbreak of a new virus, which has been named COVID-22. The virus, which is believed to have originated in China, has spread to several other countries, including the United States, Canada, and Australia. The WHO has advised countries to take necessary measures to prevent the spread of the virus and to provide support to affected individuals and communities.",
+          image : ""
+      },
+      2: {
+        headline: "Google Launches New Cloud Gaming Service",
+        content:
+          "Google has launched a new cloud gaming service called Stadia, which allows users to play high-quality video games on any device with internet access. The service uses Google's powerful cloud infrastructure to stream games directly to users' devices, eliminating the need for expensive gaming consoles or high-end computers. The service has already received positive reviews from early adopters.",
+          image : ""
+      },
+      3: {
+        headline: "Major Airlines Announce Plans to Go Carbon Neutral",
+        content: "Several major airlines, including Delta, United, and American, have announced plans to go carbon neutral by 2050. The airlines will achieve this goal by investing in renewable energy, improving fuel efficiency, and purchasing carbon offsets. The move comes as the aviation industry faces increasing pressure to reduce its carbon footprint and address the impact of air travel on the environment.",
+        image : ""
+      },
+      4: {
+        headline: "Tesla Unveils New Electric Semi-Truck",
+        content: "Tesla has announced the launch of its new electric semi-truck, which is designed to revolutionize the transportation industry. The semi-truck has a range of up to 500 miles on a single charge and can accelerate from 0 to 60 miles per hour in just 20 seconds. The vehicle is also equipped with advanced safety features and an autonomous driving system that can help reduce accidents on the road.",
+        image : ""
+      },
+      5: {
+        headline: "NASA Discovers New Planet in Habitable Zone",
+        content: "NASA has announced the discovery of a new exoplanet located in the habitable zone of a star system approximately 100 light-years away from Earth. The planet, named Kepler-438b, is similar in size to Earth and has a temperature that could potentially support liquid water on its surface. The discovery is a major breakthrough in the search for habitable worlds beyond our solar system.",
+        image : ""
+      },
+      6: {
+        headline: "Amazon Acquires MGM Studios for $8.45 Billion",
+        content: "Amazon has announced the acquisition of MGM Studios, one of the oldest and most prestigious movie studios in Hollywood, for $8.45 billion. The acquisition will give Amazon access to MGM's vast library of movies and television shows, including the James Bond franchise and popular TV series like The Handmaid's Tale. The move is expected to bolster Amazon's position in the highly competitive streaming market.",
+        image : ""
+      }
+  },
+
+  "2023-02-19": {
+    1: {
+        headline:
+          "Elon Musk Becomes World's Richest Person",
+        content:
+          "Elon Musk, the CEO of Tesla and SpaceX, has surpassed Jeff Bezos to become the world's richest person, with a net worth of over $200 billion. Musk's wealth has been driven by the soaring stock prices of Tesla and other companies he founded, as well as his bold vision for the future of space exploration and renewable energy.",
+          image : "https://cdn.britannica.com/05/236505-050-17B6E34A/Elon-Musk-2022.jpg?w=400&h=300&c=crop"
+      },
+      2: {
+        headline: "Apple Introduces New iPhone with Foldable Screen",
+        content:
+          "Apple has unveiled its latest iPhone model, which features a foldable screen that can be opened and closed like a book. The device also boasts advanced camera and video capabilities, as well as a powerful processor and improved battery life. The new iPhone is expected to be a major hit with consumers who are looking for the latest and greatest in smartphone technology.",
+          image : "https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-14-Pro-iPhone-14-Pro-Max-hero-220907.jpg.og.jpg?202302091648"
+      },
+      3: {
+        headline: "Facebook Announces Plans to Launch Metaverse",
+        content: "Facebook has announced plans to launch a new online world called the Metaverse, which will allow users to interact with each other in virtual reality. The Metaverse will be a fully immersive experience that combines elements of gaming, social media, and entertainment, and is expected to be a major new revenue stream for the company. Facebook's CEO, Mark Zuckerberg, has described the Metaverse as the next evolution of the internet, and the company has invested billions of dollars in developing the technology to make it a reality.",
+        image : "https://img.etimg.com/thumb/msid-98054653,width-294,resizemode-4,imgsize-69712/daily-news-and-latest-updates-live-19-february-2023.jpg"
+      },
+      4: {
+        headline: "Omicron Variant Causes Surge in COVID-19 Cases",
+        content: "The Omicron variant of COVID-19 has caused a sharp increase in cases around the world, leading to renewed lockdowns and travel restrictions in many countries. The variant, which is highly transmissible, has been detected in over 100 countries and has led to a surge in hospitalizations and deaths. Governments and health organizations are working to contain the spread of the virus by promoting vaccination and implementing social distancing measures.",
+        image : "https://cdn.who.int/media/images/default-source/mca/mca-covid-19/coronavirus-2.tmb-479v.jpg?sfvrsn=4dba955c_12%20479w"
+      },
+      5: {
+        headline: "China Launches First Space Station",
+        content: "China has successfully launched its first space station, marking a major milestone in the country's space program. The space station, named Tiangong, will orbit the Earth and be used for scientific research and experiments. The launch is part of China's ambitious plan to become a major player in space exploration and eventually establish a permanent manned presence on the Moon.",
+        image : "https://akm-img-a-in.tosshub.com/businesstoday/images/story/202302/china-sixteen_nine-sixteen_nine.jpg?size=948:533"
+      },
+      6: {
+        headline: "Google Announces Breakthrough in Quantum Computing",
+        content: "Google has announced a major breakthrough in the field of quantum computing, with the development of a new quantum processor that is 1,000 times more powerful than its previous processor. The new processor, named Sycamore II, is capable of performing calculations that would take traditional computers billions of years to complete. The breakthrough is expected to have major implications for a wide range of industries, including finance, cryptography, and materials science.",
+        image : "https://cdn.webrazzi.com/uploads/2022/07/google-723.png"
+      }
+  },
+};
+
+const read_date = () => {
+  let date = document.getElementById("date");
+  write_date(date);
+};
+
+const write_date = (date) => {
+  let heading_box = document.getElementsByClassName("heading");
+  let content_box = document.getElementsByClassName("content");
+  let image_box = document.getElementsByClassName("image");
+  let j = 1;
+  for (let i = 0; i < 6; i++) {
+    heading_box[i].innerHTML = news[date.value][i + 1]["headline"] + `:-`;
+    content_box[i].innerHTML = news[date.value][i + 1]["content"];
+    image_box[i].src = news[date.value][i + 1]["image"];
+    // image_box[i].height = "100 px";
+  }
+};
